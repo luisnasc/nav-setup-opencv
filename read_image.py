4f6acef8b4601cd4fefdad89d6600de87f8a8678
Luis Nascimento <luis.bruno@vixsystem.com.br>
2022-05-05 08:28:04 -0300

    Novas funcionalidades

--- read_image.py
+++ read_image.py
@@ -1,5 +1,6 @@
+from re import X
 import numpy as np
-import cv2
+import cv2, json
 import matplotlib.pyplot as plt
 from PIL import Image, ImageDraw
 import math, json
@@ -24,6 +25,10 @@ drawing = False
 graph_mode = False
 count_click = 0
 angle_mode = False
+dict_tags = {}
+objects_to_delete = []
+cont_tag = 0
+flag_iniciou_tag = 0
 
 colors = {'blue': (255, 0, 0), 'green': (0, 255, 0), 'red': (255, 0, 255), 'yellow': (0, 255, 255), 'magenta': (255, 0, 255), 'cyan': (255, 255, 0), 'white': (255, 255, 255), 'black': (0, 0, 0), 'gray': (125, 125, 125), 'rand': np.random.randint(0, high=256, size=(3,)).tolist(), 'dark_gray': (50, 50, 50), 'light_gray': (220, 220, 220)}
 
@@ -61,8 +66,12 @@ def create_circle(x, y, r, canvasName): #center coordinates, radius
 
 def draw_circle2(event):
     mouseX,mouseY = event.x,event.y
-    max_y, max_x = np.shape(img)
-    global root, image_window, seta_p1, angle_mode, tag_mode
+    shape = np.shape(img)
+    max_y, max_x  = shape[0], shape[1]
+
+
+    #max_y, max_x = np.shape(img)
+    global root, image_window, seta_p1, angle_mode, tag_mode, point1, objects_to_delete, canvas
     
     #myCanvas = tkinter.Canvas(root)
     #myCanvas.pack()
@@ -86,11 +95,43 @@ def draw_circle2(event):
             point = trans_coord([x,y])
             x,y = point[0], point[1]
             print(np.divide(point, zoom_factor))
-            create_circle(mouseX, mouseY, radius*zoom_factor, canvas)
-        except:
-            pass
+            c = create_circle(mouseX, mouseY, radius*zoom_factor, canvas)
+            objects_to_delete.append(c)
+        except Exception as e:
+            print(e)
     elif tag_mode:
+        if not angle_mode:
+            angle_mode = True
+            try:
+                # print('primeiro ponto')
+                canvas = event.widget
+                mouseX = canvas.canvasx(event.x)
+                mouseY = canvas.canvasy(event.y)
 
+                theta = np.pi
+                point = np.transpose([mouseX, mouseY, 1])
+                c, s = np.cos(float(theta)),np.sin(float(theta))
+                HT = np.array([
+                [c, 0, s], # Homogeneous Transformation Matrix
+                [0, 1, 0],
+                [-s, 0, c]])
+                point2 = np.dot(HT, point)
+
+                x,y = point2[0]*resolution, (point2[1]-max_y)*resolution
+
+                point1 = trans_coord([x,y])
+                point1 = np.divide(point1, zoom_factor)
+                seta_p1 = (mouseX, mouseY)
+                obj = canvas.create_line(seta_p1[0]+30, seta_p1[1], seta_p1[0]-30, seta_p1[1], width=3, fill='green')
+                objects_to_delete.append(obj)
+                obj = canvas.create_line(seta_p1[0], seta_p1[1]+30, seta_p1[0], seta_p1[1]-30, width=3, fill='green')
+                objects_to_delete.append(obj)                
+                obj = canvas.create_line(seta_p1[0]+2, seta_p1[1], seta_p1[0]-2, seta_p1[1], width=3, fill='red')
+                objects_to_delete.append(obj)
+                x,y = point1[0], point1[1]
+            except Exception as e:
+                print(e)
+    else:
         try:
             canvas = event.widget
             mouseX = canvas.canvasx(event.x)
@@ -106,70 +147,70 @@ def draw_circle2(event):
             point2 = np.dot(HT, point)
 
             x,y = point2[0]*resolution, (point2[1]-max_y)*resolution
-
             point = trans_coord([x,y])
-            x,y = point[0], point[1]
             print(np.divide(point, zoom_factor))
-        except:
-            pass
+        except Exception as e:
+            print(e)
+
+
+def create_seta(event):
+    mouseX,mouseY = event.x,event.y
+    shape = np.shape(img)
+    max_y, max_x  = shape[0], shape[1]
+    # max_y, max_x = np.shape(img)
+    global root, image_window, seta_p1, angle_mode, tag_mode, cont_tag, objects_to_delete
+    
+    if angle_mode:
+        angle_mode = False
+        # print('segundo ponto')
+        try:
+            canvas = event.widget
+            mouseX = canvas.canvasx(event.x)
+            mouseY = canvas.canvasy(event.y)
+            
+
+
+            theta = np.pi
+            point = np.transpose([mouseX, mouseY, 1])
+            c, s = np.cos(float(theta)),np.sin(float(theta))
+            HT = np.array([
+            [c, 0, s], # Homogeneous Transformation Matrix
+            [0, 1, 0],
+            [-s, 0, c]])
+            point2 = np.dot(HT, point)
+
+            x,y = point2[0]*resolution, (point2[1]-max_y)*resolution
+
+            point2 = trans_coord([x,y])
+            theta = math.atan2(point2[1]-point1[1], point2[0]-point1[0])
+            
+            seta_p2 = (mouseX, mouseY)
+            #print(np.divide(point, zoom_factor))
+            print(f'Posição da {cont_tag}ª Tag: {point1[0]}, {point1[1]}, {theta}')
+
+
+            dict_tags[str(cont_tag)] = [point1[0], point1[1], theta]
+
+            obj = canvas.create_line(seta_p1[0], seta_p1[1], seta_p2[0], seta_p2[1], width=3, fill='blue', arrow=tkinter.LAST)
+            objects_to_delete.append(obj)
+            obj = canvas.create_text(seta_p1[0], seta_p1[1],fill="red",font="Times 20 bold", text=str(cont_tag).zfill(4)) 
+            objects_to_delete.append(obj)
+            cont_tag+=1
+            #
+        except Exception as e:
+            print(e)
+    
 
 
 
-        if angle_mode=='False':
-            angle_mode = True
-            try:
-                print('primeiro ponto')
-                canvas = event.widget
-                mouseX = canvas.canvasx(event.x)
-                mouseY = canvas.canvasy(event.y)
-
-                theta = np.pi
-                point = np.transpose([mouseX, mouseY, 1])
-                c, s = np.cos(float(theta)),np.sin(float(theta))
-                HT = np.array([
-                [c, 0, s], # Homogeneous Transformation Matrix
-                [0, 1, 0],
-                [-s, 0, c]])
-                point2 = np.dot(HT, point)
-
-                x,y = point2[0]*resolution, (point2[1]-max_y)*resolution
-
-                point = trans_coord([x,y])
-                seta_p1 = (mouseX, mouseY)
-                x,y = point[0], point[1]
-            except:
-                pass
-
-        elif angle_mode:
-            angle_mode = False
-            print('segundo ponto')
-            try:
-                canvas = event.widget
-                mouseX = canvas.canvasx(event.x)
-                mouseY = canvas.canvasy(event.y)
-
-                theta = np.pi
-                point = np.transpose([mouseX, mouseY, 1])
-                c, s = np.cos(float(theta)),np.sin(float(theta))
-                HT = np.array([
-                [c, 0, s], # Homogeneous Transformation Matrix
-                [0, 1, 0],
-                [-s, 0, c]])
-                point2 = np.dot(HT, point)
-
-                x,y = point2[0]*resolution, (point2[1]-max_y)*resolution
-
-                point = trans_coord([x,y])
-                theta = math.atan2(p2[1]-p1[1], p2[0]-p1[0])
-                print(f'Posição da Tag: {theta}')
-                seta_p2 = (mouseX, mouseY)
-                print(np.divide(point, zoom_factor))
-
-                canvas.create_line(seta_p1[0], seta_p1[1], seta_p2[0], seta_p2[1], arrow=tkinter.LAST) 
-            except:
-                pass
-
-
+def save_as_png(canvas,fileName):
+    # save postscipt image 
+    x = canvas.canvasx(40)
+    y = canvas.canvasy(0)
+    canvas.postscript(file = fileName + '.eps', height = altura, width=largura, x=x, y=y) 
+    # use PIL to convert to PNG 
+    img = Image.open(fileName + '.eps') 
+    img.save(fileName + '.png', 'png') 
 
 init_setup()
 
@@ -179,54 +220,121 @@ img = cv2.imread(map_file, cv2.IMREAD_UNCHANGED)
 cv2.namedWindow('image')
 #cv2.setMouseCallback('image',draw_circle)
 img, global_origin = zoom(img, zoom_factor)
-altura, largura = np.shape(img)
+shape = np.shape(img)
+altura, largura  = shape[0], shape[1]
+# altura, largura = np.shape(img)
 
 def close_win():
    root.destroy()
 
 def ligar_tags():
-    global tag_mode
-    if button_tag.config('text')[-1] == 'Concluir Posicionamento':
-        button_tag.config(text='Posicionar TAGS')
-        print('habilitado')
+    global tag_mode, txt_num_tag, cont_tag, flag_iniciou_tag
+
+    if(not flag_iniciou_tag):
+        cont_tag = int(txt_num_tag.get("1.0","end-1c"))
+
+    flag_iniciou_tag = 1
+
+    if button_tag.config('text')[-1] == 'Concluir \nTAGs':
+        button_tag.config(text='Posicionar TAGs', background=defaultbg)
         tag_mode = False
+        #txt_num_tag.config(state="normal")
+        button_tag_salvar.config(state="normal")
+        button_reset.config(state="normal")
+       
     else:
-        button_tag.config(text='Concluir Posicionamento')
+        button_tag.config(text='Concluir \nTAGs', background='blue')
         tag_mode = True
+        button_graph.config(state="disabled")
+        button_tag_salvar.config(state="disabled")
+        #cont_tag = int(txt_num_tag.get("1.0","end-1c"))
+        txt_num_tag.config(state="disabled")
+        button_reset.config(state="disabled")
+
 
 def ligar_grafos():
-    global graph_mode
-    if button_graph.config('text')[-1] == 'Concluir Grafo':
-        button_graph.config(text='Criar Grafo')
+    global graph_mode, cont_tag
+
+    if button_graph.config('text')[-1] == 'Concluir \nGrafo':
+        button_graph.config(text='Criar Grafo', background=defaultbg)
         graph_mode = False
     else:
-        button_graph.config(text='Concluir Grafo')
+        button_graph.config(text='Concluir \nGrafo', background='blue')
         graph_mode = True
+
+def reset_win(event=None):
+    global image_window, im, imgtk, root, cont_tag, flag_iniciou_tag, dict_tags
+    image_window.reset_canvas(objects_to_delete)
+    flag_iniciou_tag=0
+    dict_tags = {}
+    cont_tag = int(txt_num_tag.get("1.0","end-1c"))
+    txt_num_tag.config(state="normal")
+    button_graph.config(state="normal")
+    #for i in objects_to_delete:
+    #    canvas.delete(i-1)
+    #    print(i)
+    print('FOIII!! Limpando a tela...')
+
+def salvar_tags():
+    global cont_tag, image_window, flag_iniciou_tag
+    flag_iniciou_tag = 0
+    cont_tag = int(txt_num_tag.get("1.0","end-1c"))
+    canvas_ref = image_window.cnvs
+
+    json_object = json.dumps(dict_tags, indent = 4)
+    with open("tags.json", "w") as outfile:
+        outfile.write(json_object)
+
+    save_as_png(canvas_ref,'tags_posicionadas')
+    button_graph.config(state="normal")
+    txt_num_tag.config(state="normal")
+    button_tag_salvar.config(state="disabled")
+    tkinter.messagebox.showinfo("Confirmação", "Tags posicionadas com sucesso!")
+    
 
 root = tkinter.Tk()
 root.title("Vixsystem - Setup Navegação")
 root.attributes('-zoomed', True)
-button_tag = tkinter.Button(root, text="Posicionar TAGS", width=20, height=5, command=ligar_tags)
-button_tag.place(x=230, y=20)
+defaultbg = root.cget('bg')
 
-button_graph = tkinter.Button(root, text="Configurar Grafo", width=20, height=5, command=ligar_grafos)
-button_graph.place(x=230, y=130)
-
-button_close = tkinter.Button(root, text="Sair da Aplicação", width=20, height=5, command=close_win)
-button_close.place(x=230, y=250)
-
-
-root.bind("<Button 1>",draw_circle2)
-#root.bind("<ButtonRelease-1>",create_seta)
 im = Image.fromarray(img)
 imgtk = ImageTk.PhotoImage(image=im)
 image_window = ScrollableImage(root, image=imgtk, scrollbarwidth=6, width=largura, height=altura)
+image_window.pack(side="right")
+
+labelframe = tkinter.LabelFrame(root, text="Configurar Posicionamento das Tags", width=270, height=200)
+labelframe.pack( anchor="nw" )
+ 
 
 
-#while(1):
-    #cv2.imshow('image',img)
-    #tkinter.Label(root, image=imgtk).pack() 
-image_window.pack()
+button_tag = tkinter.Button(labelframe, text="Posicionar TAGs", width=10, height=3, command=ligar_tags, wraplength=90)
+button_tag.place(x=20, y=20)
+
+button_tag_salvar = tkinter.Button(labelframe, text="Salvar TAGs", width=10, height=3, background='lightgreen', command=salvar_tags, state="disabled", wraplength=90)
+button_tag_salvar.place(x=140, y=20)
+
+lbl_num_tag = tkinter.Label(labelframe, text="Nº Tag Inicial")
+lbl_num_tag.place(x=20, y=100)
+
+txt_num_tag = tkinter.Text(labelframe, height = 1, width = 5, end="0")
+txt_num_tag.place(x=140, y=100)
+txt_num_tag.insert('end', '0')
+#left.pack()
+
+
+button_graph = tkinter.Button(root, text="Configurar \nGrafo", width=10, height=3, command=ligar_grafos,  wraplength=90)
+button_graph.place(x=20, y=210)
+
+button_reset = tkinter.Button(root, text="Resetar", width=10, height=3, command=reset_win)
+button_reset.place(x=20, y=280)
+
+button_close = tkinter.Button(root, text="Sair", width=10, height=3, command=close_win)
+button_close.place(x=20, y=360)
+
+
+root.bind("<Button 1>",draw_circle2)
+root.bind("<ButtonRelease-1>",create_seta)
+
 root.mainloop()
 # k = cv2.waitKey(20) & 0xFF
 # if k == 27:
